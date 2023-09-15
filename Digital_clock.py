064abfbb8f7426a5283ebf4e9bb466d965f8f744
Rehan Sawarn <rehansawarn@gmail.com>
2023-09-16 01:17:24 +0530

    Digital clock

---
+++ Digital_clock.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from time import *
+
+w = Tk()
+w.title("Digital clock")
+w.geometry("620x170")
+w.resizable(1,1)
+
+label = Label(w,
+    font=("Arial", 74, "bold"),
+    bg="red",
+          fg="black",
+          bd=25)
+
+label.grid(row=0, column=1)
+
+def update():
+    t = strftime("%I:%M:%S %p")
+    label.config(text=t)
+    w.after(1000, update)
+
+update()
+w.mainloop()
